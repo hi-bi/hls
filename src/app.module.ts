2fc497284586d2a114b8a72f68024bac8c5007b5
hi-bi <qds315@gmail.com>
2023-07-28 01:16:41 +0300

    feat: added entities controllers to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AlbumController, ArtistController, TrackController, UserController} from './controllers'
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AlbumController, ArtistController, TrackController,UserController],
   providers: [AppService],
 })
 export class AppModule {}
