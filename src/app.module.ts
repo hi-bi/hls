ca53ea82176b277e05d176964faef7592e2e4702
hi-bi <qds315@gmail.com>
2023-07-28 17:31:21 +0300

    feat: app.module implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AlbumController, ArtistController, TrackController, UserController} from './controllers'
+import { DataServicesModule } from './services/data-services/data-services.module';
+import { AlbumUseCasesModule } from './use-cases/album/album-use-cases.module';
+import { ArtistUseCasesModule } from './use-cases/artist/artist-use-cases.module';
+import { TrackUseCasesModule } from './use-cases/track/track-use-cases.module';
+import { UserUseCasesModule } from './use-cases/user/user-use-cases.module';
+
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    DataServicesModule,
+    AlbumUseCasesModule,
+    ArtistUseCasesModule,
+    TrackUseCasesModule,
+    UserUseCasesModule
+  ],
   controllers: [AppController, AlbumController, ArtistController, TrackController,UserController],
   providers: [AppService],
 })
