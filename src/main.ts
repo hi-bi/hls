4fec5f78b785de54d7b62a2d13626b633b5a29ce
hi-bi <qds315@gmail.com>
2023-07-29 13:56:02 +0300

    feat: handling of env variable is implemented

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  const appPort: number = parseInt(process.env.APP_PORT || '4000'); 
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(appPort);
 }
 bootstrap();
